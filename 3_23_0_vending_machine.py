352e802074486b6473309334423b430a94d06d26
agvn23 <agvn@linuxmail.org>
2026-03-23 12:06:03 +0100

    Added bank manager

---
+++ 3_23_0_vending_machine.py
@@ -0,0 +1,22 @@
+# Model real-world objects with classes and methods.
+# Use lists and sets to organize data.
+# Handle loops, nested loops, and conditionals.
+# Modularize code with functions.
+# Implement menu-driven programs with exit confirmation.
+# Track sales and stock levels dynamically.
+
+
+# Create Product – Store id, name, price, stock and sales of the product
+
+# Add Product – Store product name, price, and stock quantity.
+
+# View All Products – Display all available products with prices and stock.
+
+# Purchase Product – Select a product, check if stock is available, deduct stock, and handle payment.
+
+# Restock Product – Increase the stock quantity for a product.
+
+# Show Statistics – Total products, total stock, total sales.
+
+# Exit – Ask for confirmation before quitting.
+
